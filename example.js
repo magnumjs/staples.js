0dfce27485c08a6f28ead8191894079e6f52f02f
magnumjs <magnumjs@users.noreply.github.com>
2013-08-12 22:28:54 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,17 +1,48 @@
 // Code goes here
 
 var todosControl = function ($scope) {
-    $scope.test= 'Yo!';
-    $scope.remaining = function () {
-        return 1;
-    };
+
     $scope.todos = [{
-        text: 'first',
+        text: 'Integrate magnum',
         done: true
     }, {
-        text: 'second',
+        text: 'Leverage magnum into my app',
         done: false
     }];
+    $scope.remaining = function () {
+        var count = 0;
+        $.each($scope.todos, function (k, todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
+    };
+    $scope.size = function () {
+        return $scope.todos.length;
+    };
 }
+mag.observe().on('tmpl-begin', function (name) {
+    $('#' + name).hide();
+});
+mag.observe().on('tmpl-end', function (name) {
+    if (name == 'todosControl') {
+        $('#' + name).show();
 
-mag.control('todosControl',['test',todosControl]);
+        var ctrl = new mag.watch();
+        ctrl.secret = 'null';
+        ctrl._bind($('#todoText'), 'secret');
+        ctrl._watch($('.output'), 'secret');
+
+    }
+});
+
+mag.control('todosControl', ['test', todosControl]);
+$('.todos').addClass('more');
+setTimeout(function () {
+    mag.control('todosControl', ['',
+        function ($scope) {
+ 
+            $scope.todos[0].done = false;
+        }
+    ]);
+
+}, 1000);
